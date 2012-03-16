202747ebcec955c87d0f7169ab2fd4aace11a4c7
Blake Borgeson <borgeson@utexas.edu>
2012-03-16 15:48:32 -0500

    roc plotting

--- cv.py
+++ cv.py
@@ -1,4 +1,6 @@
-def rank_pairs(scores, sample_frac=.1):
+from __future__ import division
+
+def rank_scores(scores, sample_frac=.1):
     import random
     ranked_scores = []
     for i in range(scores.shape[0]):
@@ -8,13 +10,48 @@ def rank_pairs(scores, sample_frac=.1):
     ranked_scores.sort(key = lambda x: x[2], reverse = True)
     return ranked_scores
 
-def test_pairs(scores, true_pairs, sample_frac=.1):
+def test_pairs(scores, true_pairs, genes, sample_frac=.1):
     # scores: input 2d array of scores for each index-index pair
     # true_pairs: dict: {gene1: set(gene2,gene3,gene4), gene2:
     #   set(gene1,gene5)}
     ranked = rank_scores(scores, sample_frac)
     tested = []
     for i,j,score in ranked:
-        hit = int(j in true_pairs.get(i,[]))
-        tested.append( (i, j, score, hit) )
+        if i!=j: # remove same-same
+            hit = int(genes[j] in true_pairs.get(genes[i],[]))
+            tested.append( (i, j, score, hit) )
     return tested
+
+def roc(tested_pairs):
+    # tested pairs: [ (row, col, score, hit(0/1)), ...]
+    x = 0
+    y = 0
+    xs = [x]
+    ys = [y]
+    score_prev = 0
+    for _,_,score,hit in tested_pairs:
+        if hit==1:
+            y += 1
+        else:
+            x += 1
+        # We have this conditional so that segments with equal probability
+        # are drawn as a line with a + at the end.
+        if score != score_prev:
+            xs.append(x)
+            ys.append(y)
+            score_prev = score
+    # don't forget to add the last point
+    xs.append(x)
+    ys.append(y)
+    return xs,ys
+
+def auroc(xs,ys):
+    auroc = 0
+    xprev = 0
+    yprev = 0
+    for x,y in zip(xs,ys):
+        auroc += yprev*(x-xprev)
+        xprev = x
+        yprev = y
+    auroc = auroc / (x*y)
+    return auroc
--- plotting.py
+++ plotting.py
@@ -1,7 +1,8 @@
 import pylab
 from pylab import *
-import hcluster
 import random
+import hcluster
+import cv
 
 def cluster(corr):
     # corr: a matrix of similarity scores, such as a covariance matrix
@@ -17,3 +18,15 @@ def cluster(corr):
             print 'HEY!! probable clustering failure.'
             break
     return order
+
+def roc_plot(tested_pairs, **kwargs):
+    xs,ys = cv.roc(tested_pairs)
+    plot(xs, ys, label=' %.3f' % cv.auroc(xs,ys), **kwargs)
+    plot([0,xs[-1]], [0,ys[-1]], '--')
+
+def imshow2(*args):
+    imshow(*args, interpolation='nearest', aspect='auto',
+           cmap='bone', vmin=0)
+           # Need vmin = 0 so the lowest values aren't represented 
+           # as say black by the colorbar if they're not 0.
+           # Colormaps: bone, gray
