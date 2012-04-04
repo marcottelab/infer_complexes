fdc5efc03c219d5951e8301db33dc98226f40a62
Blake Borgeson <borgeson@utexas.edu>
2012-04-04 15:13:57 -0500

    simpler calculation of correlation scores, and scriptifying the code for running correlation on files

--- corr.py
+++ corr.py
@@ -1,14 +1,7 @@
+import sys
 import numpy as np
-
-def coelution(elut, norm=1):
-    # elut.mat: float matrix of spectral counts
-    # elut.genes: gene labels for the rows of the array
-    if norm:
-        # stupid simple: pearson correlation matrix
-        corr = np.corrcoef(elut.mat) # between -1 and 1
-    else:
-        corr = np.cov(elut.mat)
-    return corr
+import operator
+import elution as el
 
 def traver_corr(mat, repeat=200, norm='columns'):
     # As described in supplementary information in paper.
@@ -16,7 +9,7 @@ def traver_corr(mat, repeat=200, norm='columns'):
     # where A = the observed count and M is the total fractions
     # normalize each column to sum to 1
     # then correlate, and average together for repeat tries.
-    def poisson_corr(mat, iteration_display, norm=True):
+    def poisson_corr(mat, iteration_display, norm):
         print iteration_display
         M = mat.shape[1]
         C = mat + 1/M
@@ -33,4 +26,59 @@ def traver_corr(mat, repeat=200, norm='columns'):
     avg_result = (reduce(operator.add, (poisson_corr(mat, i, norm=norm) for i in
                                         range(repeat))) / repeat)
     return avg_result
+
+
+class ApexScores(object):
+
+    def __init__(self, elution):
+        mat = elution.mat
+        self.apex_array = np.array(np.argmax(mat, axis=1))
+        self.shape = (len(self.apex_array),len(self.apex_array))
+
+    def __getitem__(self, index):
+        return int(self.apex_array[index[0]] == self.apex_array[index[1]])
+
+def apex_corr(elution):
+    return ApexScores(elution)
+
+def precalc_scores(elution,extension):
+    return np.loadtxt(elution.filename + '.' + extension)
+
+
+class CosineLazyScores(object):
+
+    def __init__(self,elution):
+        mat = elution.mat
+        norms = np.apply_along_axis(np.linalg.norm, 1, mat)
+        self.mat_rownormed = np.nan_to_num(mat / np.matrix(norms).T)
+        assert type(self.mat_rownormed) == type(np.matrix(''))
+        self.shape = (mat.shape[0],mat.shape[0])
+        
+    def __getitem__(self, index):
+        # Dot product of normed rows
+        return float(self.mat_rownormed[index[0],:] *
+                    self.mat_rownormed[index[1],:].T)
+
+def cosine_lazy(elution):
+    return CosineLazyScores(elution)
+
+if __name__ == '__main__':
+    nargs = len(sys.argv)
+    if nargs < 3:
+        sys.exit("usage: python corr.py filename method(poisson|dotproduct|corrcoef|cov) [argument]") 
+    fname = sys.argv[1]
+    method = sys.argv[2]
+    methodarg = None if nargs < 4 else sys.argv[3]
+    elut = el.load_elution(fname)
+    if method == 'poisson':
+        corr = traver_corr(elut.mat, repeat=methodarg) if methodarg else \
+            traver_corr(elut.mat)
+    elif method == 'dotproduct':
+        corr = elut.mat * elut.mat.T
+    elif method == 'corrcoef':
+        corr = np.corrcoef(elut.mat)
+    elif method == 'cov':
+        corr = np.cov(elut.mat)
+    fileout = fname+'.corr_'+method
+    np.savetxt(fileout, corr, delimiter='\t')
 
