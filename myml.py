b9ca89187a4452ddba630e2809da3482525a719b
Blake Borgeson <git@blakeb.org>
2012-05-15 22:47:55 -0500

    Using sklearn svm

---
+++ myml.py
@@ -0,0 +1,30 @@
+import plotting as pl
+from sklearn import svm
+
+#extr, exte = ml.full_examples('Ce_ensp', fs[:1], ['poisson'], 'Ce',
+#'cep2ceg.tab')
+
+def fit_and_score(exs_train, exs_test):
+    classer = fit_svm(exs_train)
+    tested = score_svm(classer, exs_test)
+    return tested
+
+def fit_svm(exs_train):
+    classer = svm.SVC(kernel='linear', probability=True) 
+    _,_,X,y = extract_data_labels(exs_train.examples)
+    classer.fit(X,y)
+    return classer
+
+def score_svm(classer, exs_test):
+    p1s,p2s,X0,y0 = extract_data_labels(exs_test.examples)
+    y0p = [x[1] for x in classer.predict_proba(X0)]
+    tested = [(p1,p2,score,label) for p1,p2,score,label in zip(p1s,p2s,y0p,y0)]
+    tested.sort(key=lambda x:x[2],reverse=True)
+    return tested
+
+def extract_data_labels(ex_list):
+    p1 = [e[0] for e in ex_list]
+    p2 = [e[1] for e in ex_list]
+    X = [[0 if x == '?' else x for x in e[3:]] for e in ex_list]
+    y = [int(e[2]=='true') for e in ex_list]
+    return p1,p2,X,y
