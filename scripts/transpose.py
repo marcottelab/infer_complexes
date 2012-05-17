173c967287c6dc9d8a2233dd76d98e1373d13863
Blake Borgeson <git@blakeb.org>
2012-05-17 15:52:32 -0500

    came out of syn somehow.  Frustrating.

--- scripts/transpose.py
+++ scripts/transpose.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+
 def transpose(d, fin, fout):
     sys.path.append(d+'/..')
     import utils as ut
@@ -9,7 +10,13 @@ def transpose(d, fin, fout):
     if endindex: print "skipping last line"
     lines = [l for l in ut.load_tab_file(fin)][:endindex]
     cols = ut.zip_exact(*lines) #zip messes up if these files aren't neat
+    # _After_ zipping, get rid of the column 1 header--R doesn't like it.
+    col0list = list(cols[0])
+    assert col0list[0][0] == '#' # make sure we're removing what we should be
+    col0list.remove(col0list[0])
+    cols[0] = tuple(col0list)
     col2title = cols[1][0].lower()
+    # get rid of the total/descr column
     if col2title.find('total') > -1 or col2title.find('descr') > -1:
         cols.remove(cols[1])
         print "removing second column--extraneous"
