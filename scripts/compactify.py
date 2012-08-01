cc9a0953d509566bbe583d8433f471b25f4a8282
Blake Borgeson <git@blakeb.org>
2012-08-01 18:56:48 -0500

    quick script to compact a float array file down to f2

---
+++ scripts/compactify.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import numpy as np
+
+def compact(d, scoref, dtype):
+    sys.path.append(d+'/..')
+    import utils as ut
+    compactf = '%s.%s.pyd' % (scoref, dtype)
+    print compactf, dtype
+    ascores = np.loadtxt(scoref, dtype='f2')
+    ut.savepy(ascores, compactf)
+
+if __name__ == '__main__':
+    nargs = len(sys.argv)
+    if nargs < 2:
+        sys.exit("usage: python compactify.py fname dtype")
+    d = os.path.dirname(sys.argv[0])
+    fname = sys.argv[1]
+    dtype = sys.argv[2]
+    compact(d, fname, dtype)
