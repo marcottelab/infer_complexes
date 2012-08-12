194ec7276044c6b790edb361de810dbe627ee926
Blake Borgeson <git@blakeb.org>
2012-08-12 16:21:45 -0500

    Calculate species counts when filtering for n species, plus some function renaming

--- myml.py
+++ myml.py
@@ -13,37 +13,41 @@ from ppi import filter_arr
 import numpy as np
 import multiprocessing
 import operator
+import pairdict as pd
 
 NCORES = multiprocessing.cpu_count()
 NET_SPS = 'HS CE DM SC'.split()
 
-def fit_and_test(scored, classer, columns=None, cutoff=0.25): 
+def fit_and_test(scored, clf, columns=None, cutoff=0.25): 
     """
     scored: [arr_train, arr_test]
     """
     arr_train, arr_test = scored
-    classer = fit_svm(arr_train, classer, columns)
-    tested = test_svm(classer, arr_test, columns)
+    fit_clf(arr_train, clf, columns)
+    tested = classify(clf, arr_test, columns)
     return tested
 
-def fit_svm(arr_train, classer, columns=None, cutoff=0.25):
+def fit_clf(arr_train, clfbase, columns=None, cutoff=0.25):
     arr_train, names = filter_names_arr(arr_train, columns, cutoff)
-    classer.fit(features(arr_train, names), arr_train['hit'])
-    return classer
+    print "Training classifier: %s examples, %s features" % (len(arr_train),
+        len(names))
+    clfbase.fit(features(arr_train, names), arr_train['hit'])
 
-def test_svm(classer, arr_test, columns=None, cutoff=0.25, savef=None):
+def classify(clf, arr_test, columns=None, cutoff=0.25, savef=None):
     arr_test, names = filter_names_arr(arr_test, columns, cutoff)
     if columns: print 'Features:', ut.count_collect(names, 6)
-    probs = (x[1] for x in classer.predict_proba(features(arr_test, names)))
+    print "Classifying: %s examples, %s features" % (len(arr_test), 
+            len(names))
+    probs = (x[1] for x in clf.predict_proba(features(arr_test, names)))
     tested = zip(arr_test['id1'], arr_test['id2'], probs, arr_test['hit'])
     tested.sort(key=lambda x:x[2],reverse=True)
     if savef: ut.savepy(tested, savef)
     return tested
 
-def test_svm_slice(classer,arr_test, perslice, savef=None, columns=None,
+def classify_slice(clf,arr_test, perslice, savef=None, columns=None,
         cutoff=0.25, maintain=True, startslice=0):
     nslices = int(np.ceil(len(arr_test) / perslice))
-    slices = (test_svm(classer, arr_test[i*perslice:(i+1)*perslice],
+    slices = (classify(clf, arr_test[i*perslice:(i+1)*perslice],
                        savef=(savef+str(i)+'.pyd') if savef else None,
                        columns=columns, cutoff=cutoff)
               for i in range(startslice, nslices))
@@ -143,6 +147,8 @@ def filter_nsp(arr, nsp=2, cutoff=0.25, dontfilt=True, count_ext=True):
     """
     Filter to only leave interactions for which evidence exists in nsp species.
     scores: [arr_train, arr_test]
+    In addition to returning the filtered array, returns a matching pairdict of
+    species counts.
     """
     if len(arr) == 2:
         # user provided [trainarr, testarr]
@@ -159,13 +165,17 @@ def filter_nsp(arr, nsp=2, cutoff=0.25, dontfilt=True, count_ext=True):
     spcols = [[f for f in features 
                 if f[:2]==s or (count_ext==True and f[:2]=='ex' and f[4:6]==s)]
                 for s in sps]
-    # useful if adding new data and debugging
-    # print spcols
+    # Note that maxes are transposed relative to what might be expected.
     maxes = [[max(i) for i in arr[scs]] for scs in spcols]
     exceed_inds = [i for i in range(len(arr))
                    if len([i for m in maxes if m[i] > cutoff]) >= nsp]
-    return arr[exceed_inds]
-    
+    sp_counts = [c for c in [len([1 for m in maxes if m[i] > cutoff]) 
+        for i in range(len(arr))] if c >= nsp]
+    arrfilt = arr[exceed_inds]
+    pd_spcounts = pd.PairDict([[arrfilt[i][0],arrfilt[i][1],sp_counts[i]] 
+                            for i in range(len(arrfilt))])
+    return arr[exceed_inds], pd_spcounts
+
 if __name__ == '__main__':
     nargs = len(sys.argv)
     if nargs < 6:
@@ -178,5 +188,5 @@ if __name__ == '__main__':
     path = sys.argv[5]
     exs_slice = exs[i*perslice:(i+1)*perslice]
     del exs
-    test_svm_slice(clf, exs_slice, 100000, savef=(path+str(i)+'_'),
+    classify_slice(clf, exs_slice, 100000, savef=(path+str(i)+'_'),
             maintain=False, startslice=0)
