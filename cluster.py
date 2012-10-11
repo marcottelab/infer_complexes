01e3cfabe2fa533eeea93f2e22ca8fe3bf12eeb3
Blake Borgeson <git@blakeb.org>
2012-10-10 20:07:38 -0400

    Clustering: some reorganization, added mcl, remove dupes.

--- cluster.py
+++ cluster.py
@@ -6,17 +6,22 @@ import corum as co
 import utils as ut
 import pairdict
 
-def filter_c1(tested, negmult=50, **kwargs):
-    cxs = cluster_one(tested, negmult, **kwargs)
+mcl_command = '%(mclpath)s %(fin)s -I %(I)s -o %(fout)s --abc'
+c1_command = 'java -jar %(c1path)s -s %(min_size)s -d %(min_density)s --haircut %(haircut)s --penalty %(penalty)s --max-overlap %(max_overlap)s %(fluff)s %(fin)s > %(fout)s '
+def filter_clust(tested, negmult=50, command=mcl_command, do_dedupe=True,
+        **kwargs):
+    cxs = cluster(tested, negmult, command, **kwargs)
+    if do_dedupe: cxs = dedupe(cxs)
+    return cxs, _filter_ints(tested, cxs)
+    
+def _filter_ints(inlist, cxs):
     d_cxs = dict([(i,set(c)) for i,c in enumerate(cxs)]) 
     pairs = co.pairs_from_complexes(d_cxs)
     pd = pairdict.PairDict(pairs)
-    return cxs, _filter_ints(tested, pd)
-    
-def _filter_ints(inlist, pd):
     return [tup for tup in inlist if pd.contains((tup[0],tup[1]))]
-    
-c1defaults = {
+
+clust_defaults = {
+    'I': 1.8,
     'min_size': 2,
     'min_density': 0.2,
     'haircut': 0.0,
@@ -24,6 +29,7 @@ c1defaults = {
     'max_overlap': 0.6,
     'fluff': '--no-fluff',
     'c1path': os.path.expanduser('~/Dropbox/complex/tools')+'/cluster_one.jar',
+    'mclpath': os.path.expanduser('~/local/bin')+'/mcl',
     'fin':
         os.path.expanduser('~/bigdata/complex/predictions')+
         '/temp_interactions.tab',
@@ -39,14 +45,13 @@ def n_thresh(tested, score):
         if t[2] < score:
             return i
     
-def cluster_one(tested, negmult, **kwargs):
-    kwargs = set_defaults(kwargs, c1defaults)
-    export_c1(tested, kwargs['fin'], negmult)
-    command = 'java -jar %(c1path)s -s %(min_size)s -d %(min_density)s --haircut %(haircut)s --penalty %(penalty)s --max-overlap %(max_overlap)s %(fluff)s %(fin)s > %(fout)s ' % kwargs
+def cluster(tested, negmult, command, **kwargs):
+    kwargs = set_defaults(kwargs, clust_defaults)
+    export_cxs(tested, kwargs['fin'], negmult)
     print command
-    shell_call(command)
-    ut.printnow('finished cluster_one')
-    cxs = ut.load_list_of_lists(kwargs['fout'])
+    shell_call(command % kwargs)
+    ut.printnow('finished clustering')
+    cxs = [set(c) for c in ut.load_list_of_lists(kwargs['fout'])]
     return cxs
 
 def set_defaults(d, defaultd):
@@ -55,7 +60,7 @@ def set_defaults(d, defaultd):
             d[k] = v
     return d
     
-def export_c1(tested, fname, negmult):
+def export_cxs(tested, fname, negmult):
     ut.write_tab_file([(t[0], t[1], ut.rescale(float(t[2]),negmult)) for t in
         tested], fname)
     
@@ -69,3 +74,14 @@ def shell_call(command):
     #return subprocess.check_call(command,  shell=True)
     # use check_output to return the output instead of streaming it
     #return subprocess.check_output(command, shell=True)
+
+
+def remove_dupes(cxs):
+    """
+    cxs: list of sets
+    """
+    deduped = []
+    for c in cxs:
+        if c not in deduped:
+            deduped.append(c)
+    return deduped
