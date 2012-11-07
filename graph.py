5bcd5bf14df6d833b5c7e232ca901be725e92fa7
Blake Borgeson <git@blakeb.org>
2012-11-07 15:53:19 -0600

    networkx analysis of cliques in graph

--- graph.py
+++ graph.py
@@ -1,7 +1,10 @@
 from __future__ import division
+import itertools as it
+from collections import defaultdict
+import numpy as np
+import networkx as nx
 import utils as ut
 import pairdict as pd
-import itertools as it
 
 def poss_ints(nps):
     return (nps*(nps-1))/2
@@ -17,4 +20,17 @@ def cliqueness(cx,pdppis):
 def filter_cxs(cxs, cxppis, qness=.7, min_size=0):
     pdppis = pd.PairDict(cxppis)
     return [c for c in cxs if cliqueness(c,pdppis)>qness and len(c)>=min_size]
+
+def nx_cliques(ppis, min_len=3, min_weight=0):
+    G = nx.Graph()
+    G.add_weighted_edges_from([p[:3] for p in ppis])
+    qs = [set(c) for c in nx.find_cliques(G) if len(c) >= min_len]
+    if min_weight:
+        qs = [q for q in qs if avg_weight(G,q) > min_weight]
+    return qs
+
+def avg_weight(G, clique):
+    return np.mean([G[p1][p2]['weight'] 
+        for p1,p2 in it.combinations(clique,2)])
+
 
