568782b57f5d8fde7ade233b3e7508edee14fe0d
Blake Borgeson <git@blakeb.org>
2013-02-18 16:48:49 -0600

    Mapping filenames to fractionation folders: try for generalization a little

--- scripts/mv_fractions2folders.py
+++ scripts/mv_fractions2folders.py
@@ -4,11 +4,14 @@ import sys
 sys.path.append(os.path.dirname(abspath(__file__))+'/../')
 import utils as ut
 
-def maybe_move(fpath, file2folder):
+def maybe_move(fpath, file2folder, remove_final_underscore=True):
     if not os.path.exists(fpath):
         print "File not found:", fpath
         return
     basename = ut.shortname(fpath).split('.')[0]
+    if remove_final_underscore:
+        basename = ('_'.join(basename.split('_')[:3]) 
+                if len(basename.split('_'))>2 else basename)
     if not basename in file2folder:
         print "No mapping for file:", fpath, basename
         return 
