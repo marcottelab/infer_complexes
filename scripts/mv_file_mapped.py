57050b14876a54121a126e128ac54225fe8e3660
Blake Borgeson <git@blakeb.org>
2013-02-19 16:32:13 -0600

    renaming some raw files

---
+++ scripts/mv_file_mapped.py
@@ -0,0 +1,25 @@
+import os
+from os.path import abspath
+import sys
+sys.path.append(os.path.dirname(abspath(__file__))+'/../')
+import utils as ut
+
+def move(fname, fmap):
+    basename = ut.shortname(fname)
+    fext = os.path.splitext(fname)[1]
+    fdir = os.path.split(fname)[0]
+    if basename in fmap:
+        newname = os.path.join(fdir,fmap[basename] + fext)
+        print "moving", fname, newname
+        os.rename(fname, newname)
+    else:
+        print "not found", fname
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit("usage: python blah.py mapfile.txt filename(s)") 
+    fname_map = sys.argv[1]
+    filenames = sys.argv[2:]
+    fmap = dict(ut.load_tab_file(fname_map))
+    for f in filenames:
+        move(f, fmap)
