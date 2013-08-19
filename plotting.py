055a0cb014ca66cfc0f0a763328ba6ae43575d2e
Blake Borgeson <git@blakeb.org>
2013-08-19 09:53:51 -0500

    Lots of new plotting functions

--- plotting.py
+++ plotting.py
@@ -33,12 +33,17 @@ def plot_result(result, ppis=None, **kwargs):
 
 def boot_resample(extr_exte):
     return [Struct(names=ex.names,examples=ut.sample_wr(ex.examples, len(ex.examples))) for ex in extr_exte]
-    
-def rolling_scores(tested, show=1000, window=50, rescale=0, **kwargs):
+
+def rolling_scores(tested, true_ints=None, show=1000, window=50, rescale=0,
+        **kwargs):
     #rolling = [len([t for t in tested[i:i+window] if t[3]==1])/window for i in
     #range(show-window)]
     if rescale > 0:
         tested = [(t[0],t[1],ut.rescale(t[2],rescale), t[3]) for t in tested]
+    if true_ints:
+        pdtrues = pd.PairDict(true_ints)
+        tested = [(p[0],p[1],p[2],1 if pdtrues.contains(p[:2]) else 0) for p
+                in tested]
     padded = list(np.zeros((50,4)))+list(tested)
     rolling = [len([t for t in padded[i:i+window] if t[3]==1])/window for i in range(show)]
     #plot([0]*window + rolling, **kwargs)
@@ -75,11 +80,13 @@ def bar_plot(names, yvals, slanted_names=False, **kwargs):
 
 def roc_plot(cvpairs, **kwargs):
     xs,ys = cv.roc(cvpairs) 
-    kwargs['label'] = kwargs.get('label','') + ' %.3f' % cv.auroc(xs,ys)
+    auroc = cv.auroc(xs,ys)
+    kwargs['label'] = kwargs.get('label','') + ' %.3f' % auroc
     plot(xs, ys, **kwargs)
     plot([0,xs[-1]], [0,ys[-1]], 'k--')
-    
-def pr_plot(cv_pairs, total_trues, rescale=None, prec_test=None, **kwargs):
+
+def pr_plot(cv_pairs, total_trues, rescale=None, prec_test=None,
+        true_ints=None, **kwargs):
     """
     rescale: adjust precision values assuming rescale times as many negatives
     total_trues:
@@ -87,6 +94,10 @@ def pr_plot(cv_pairs, total_trues, rescale=None, prec_test=None, **kwargs):
     - 'auto' to calculate from the supplied tested cv_pairs
     - integer to use that supplied integer as total trues
     """
+    if true_ints:
+        pdtrues = pd.PairDict(true_ints)
+        cv_pairs = [(p[0],p[1],p[2],1 if pdtrues.contains(tuple(p[:2])) else 0) for p
+                in cv_pairs]
     if total_trues == 'auto':
         total_trues = len([t for t in cv_pairs if t[3]==1])
     recall,precision = cv.pr(cv_pairs) 
@@ -95,7 +106,7 @@ def pr_plot(cv_pairs, total_trues, rescale=None, prec_test=None, **kwargs):
     if prec_test:
         kwargs['label'] = kwargs.get('label','') + (' Re:%0.2f' %
         cv.calc_recall(precision,prec_test, total_trues)) + (' @ Pr:%0.2f'
-            % precision_test)
+            % prec_test)
     if total_trues:
         recall = [r/total_trues for r in recall]
     plot(recall, precision, **kwargs)
@@ -112,20 +123,42 @@ def imshow2(*args):
            # as say black by the colorbar if they're not 0.
            # Colormaps: bone, gray
 
-def examples_dist_arr(arr, score_indices, uselog=True, normed=True, default=-1,
-                   missing='?', linewidth=3, histtype='step', ncols=1,
-                   **kwargs):
-    nplots = len(score_indices)+1
-    pos,neg = [arr[[i for i in range(len(arr)) if arr[i][2]==t]] for t in 1,0]
+def examples_dist_arr(arr, score_indices, ncols=1, use_legend=False, **kwargs):
+    extra = 1 if use_legend else 0
+    nplots = len(score_indices)+extra
+    pos,neg = pos_neg_from_arr(arr)
     for i, (ind, name) in enumerate([(ind,arr.dtype.names[ind]) for ind in score_indices]):
-        subplot(int(nplots/ncols)+1, ncols, i+2)
+        subplot(int(nplots/ncols)+extra, ncols, i+1+extra)
+        hp,hn = examples_dist_single(pos_neg=(pos,neg), name=name, **kwargs)
         #kwargs['range'] = kwargs['range'] if 'range' in kwargs else \ [func([func(data) for data in [pos,neg]]) for func in [min,max]]
-        kwargs['bins'] = 30 if not 'bins' in kwargs else kwargs['bins']
-        (_,_,hp),(_,_,hn) = [hist(data, log=uselog, histtype='step', linewidth=linewidth, normed=normed, **kwargs) for data in pos[name],neg[name]]
         title(name)
-    subplot(int(nplots/ncols)+2,ncols,1)
-    legend([hp[0],hn[0]],['Pos','Neg'])
-        
+    if use_legend and ((not 'odds' in kwargs) or (not kwargs['odds'])): 
+        subplot(int(nplots/ncols)+2,ncols,1)
+        legend([hp[0],hn[0]],['Pos','Neg'])
+
+def pos_neg_from_arr(arr):
+    pos,neg = [arr[[i for i in range(len(arr)) if arr[i][2]==t]] for t in 1,0]
+    return pos,neg
+
+def examples_dist_single(arr=None, pos_neg=None, name='', uselog=True,
+        normed=True, default=-1, missing='?', linewidth=3, histtype='step',
+        ncols=1, odds=False, **kwargs):
+    pos,neg = pos_neg or pos_neg_from_arr(arr)
+    kwargs['bins'] = 30 if not 'bins' in kwargs else kwargs['bins']
+    if odds:
+        phist,nhist = [np.histogram(data[name], range=(-1,1), density=True,
+            **kwargs) for data in pos,neg]
+        yvals = [np.nan_to_num(p/n) if n>0 else 0 
+                for p,n in zip(phist[0],nhist[0])]
+        xvals = phist[1]
+        binwidth = 2/kwargs['bins']
+        plot(np.ravel(zip(xvals[:-1],xvals[:-1]+binwidth)),
+                np.ravel(zip(yvals,yvals)))
+        return None,None
+    else:
+        (_,_,hp),(_,_,hn) = [hist(data, log=uselog, histtype='step', linewidth=linewidth, normed=normed, **kwargs) for data in pos[name],neg[name]]
+        return hp,hn
+
 def presentation_mode(color='white', on=True):
     # customize individually with mpl.rcParams['text.color'] = '#000000'
     normmode = {
@@ -179,9 +212,10 @@ def ppis_scatter(ppis1, ppis2, useinds=range(3)):
             bdefaults=[0]*nvals)
     vals = zip(*ut.i1(pdcomb.d.items()))
     v1s,v2s = zip(*vals[:nvals]), zip(*vals[nvals:])
+    v1s,v2s = [ut.i0(x) for x in v1s,v2s]
     return v1s,v2s
 
-def eluts_scatter(avals, alabels, bvals, blabels):
+def scatter_union_labeled(avals, alabels, bvals, blabels):
     """
     vals are the columns of data to scatter (eg, el.mat[:,0]).  
     labels are el.prots.
@@ -191,6 +225,15 @@ def eluts_scatter(avals, alabels, bvals, blabels):
     dfout = dfs[0].join(dfs[1], how='outer', rsuffix='_b')
     dfout = dfout.fillna(0)
     return dfout.values[:,0],dfout.values[:,1]
+
+def eluts_scatter(elut1, elut2, ncols=None):
+    vals1,vals2 = [],[]
+    for icol in range(elut1.mat.shape[1])[:ncols]:
+        new1, new2 = scatter_union_labeled(elut1.mat[:,icol], elut1.prots,
+                elut2.mat[:,icol], elut2.prots)
+        vals1 = np.concatenate((vals1, new1))
+        vals2 = np.concatenate((vals2, new2))
+    return vals1,vals2
 
 def cluster_elut(mat):
     ymat = hcluster.pdist(mat)
@@ -213,7 +256,9 @@ def profiles_cxs(e, cxs, **kwargs):
     return vals
 
 def scatter_blake(a, b, which='circles', classes=[0,1], colors=['k','r'],
-        **kwargs):
+        maxval=None, **kwargs):
+    if maxval:
+        a,b = filter_valpairs(a,b,maxval)
     defaults = {'s': 50, 'alpha':.2, 'lw':0}
     kwargs = ut.dict_set_defaults(kwargs, defaults)
     if type(a[0]) == list or type(a[0]) == tuple:
@@ -240,10 +285,15 @@ def scatter_blake(a, b, which='circles', classes=[0,1], colors=['k','r'],
         scatter(a, b, facecolors='none', edgecolors=c, **kwargs)
     title('R-squared: %0.3f' % ut.r_squared(a,b))
 
-def hexbin_blake(a, b, **kwargs):
-    defaults = {'cmap': 'binary', 'bins':'log'}
+def hexbin_blake(a, b, maxval=None, **kwargs):
+    defaults = {'cmap': 'binary', 'bins':'log', 'gridsize':np.floor(sqrt(len(a))/2) }
     kwargs = ut.dict_set_defaults(kwargs, defaults)
-    hexbin(a, b, gridsize=np.floor(sqrt(len(a))/2), **kwargs)
+    if maxval:
+        a,b = filter_valpairs(a,b,maxval)
+    hexbin(a, b, **kwargs)
+
+def filter_valpairs(a,b,maxval):
+    return zip(*[(x,y) for x,y in zip(a,b) if x<maxval and y<maxval])
 
 def multi_scatter(comps,scatter_func=scatter_blake, preprocess=None,
         names=None, **kwargs):
@@ -267,4 +317,17 @@ def multi_scatter(comps,scatter_func=scatter_blake, preprocess=None,
                 ylabel(names[i])
                 xlabel(names[j])
 
+def hist_ndarray(arr, names=None, showindex=None, markerstyle='kp',
+        do_hist=True, **kwargs):
+    names = names or arr.dtype.names
+    K = len(names)
+    for i,name in enumerate(names):
+        subplot(K,1,i+1)
+        if showindex is not None:
+            plot(arr[name][showindex], 0, markerstyle, ms=15)
+        if do_hist:
+            hist(arr[name], **kwargs)
+            legend([name], numpoints=1, loc=2)
+            xlabel(name)
+        #ylabel(name)
 
