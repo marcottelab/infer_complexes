e68c8945832044607148847f3928eb7d99115214
Blake Borgeson <git@blakeb.org>
2013-10-30 16:21:34 -0500

    Generate tables of orthology conservation counts between species from a starting set of proteins.

--- count_orthologs.py
+++ count_orthologs.py
@@ -1,8 +1,10 @@
+from __future__ import division
 import elution as el
 import orth
 import seqs
 import utils as ut
 from pandas import DataFrame
+from pandas import ExcelWriter
 
 def orth_count_ogroups(sp1, sp2):
     """
@@ -37,11 +39,56 @@ def all_genes(sp):
     return ps
 
 
-def df(result, sps):
+def df_sps(result, sps):
     return DataFrame(data=result, index=sps, columns=sps)
+
+def set_from_pairs(ppis):
+    return set(ut.i0(ppis) + ut.i1(ppis))
 
 def manysp_all_prots(sps, elutfs, **kwargs):
     d_allprots = dict([(s,el.all_prots(elutfs, sp_base=s, **kwargs)) 
         for s in sps])
     return d_allprots
+
+def found_orth_prots(base_set_prots, sp, base_sp):
+    def isfound(d, key, set_values):
+        for v in d.get(key, []):
+            if v in set_values:
+                return True
+        return False
+    od = orth.odict(sp, base_sp)
+    if sp != base_sp:
+        return set([g for g in all_genes(sp) if isfound(od, g, base_set_prots)])
+    else:
+        return base_set_prots
+
+def manysp_protsets(sps, elutfs, lists_ppis, base_sp):
+    """
+    Orthology sharing computed from several sets of proteins:
+    - Fasta files
+    - In elution files, spectral count >=1
+    - " " spectral count >=2
+    - In each provided list of ppis [top_ppis, cxppis]
+    """
+    sp_prot_dicts = [
+        dict([(sp, set(all_genes(sp))) for sp in sps]), 
+        manysp_all_prots(sps, elutfs, min_count=1),
+        manysp_all_prots(sps, elutfs, min_count=2)
+        ]
+    for ppis in lists_ppis:
+        base_set_prots = set_from_pairs(ppis)
+        dprots = dict([(sp, found_orth_prots(base_set_prots, sp, base_sp)) 
+            for sp in sps])
+        sp_prot_dicts.append(dprots)
+    for d in sp_prot_dicts: 
+        print "%s length:" % base_sp, len(d[base_sp])
+    return sp_prot_dicts
+
+def write_xls_sheets(dataframes, fname, sheet_names=None):
+    writer = ExcelWriter(fname)
+    sheet_names = sheet_names or ['sheet %s' %n for n in range(len(results))]
+    for df,name in zip(dataframes, sheet_names):
+        df.to_excel(writer, name)
+    writer.save()
+
 
